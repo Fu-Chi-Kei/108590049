bf45ebd4fb4aba77428147be55b2c02038e602cc
Fu-Chi-Kei <79504942+Fu-Chi-Kei@users.noreply.github.com>
2022-11-02 19:06:04 +0800

    Add files via upload

---
+++ LAB/LAB05/script.js
@@ -0,0 +1,36 @@
+//write your script here
+function changeWidth200px(){
+document.getElementById('mytable').width="200";
+}
+function changeWidth500px(){
+document.getElementById('mytable').width="500";
+}
+
+function changeBorder1px(){
+document.getElementById('mytable').border="1";
+}
+function changeBorder10px(){
+document.getElementById('mytable').border="10";
+}
+function changeBorder20px(){
+document.getElementById('mytable').border="20";
+}
+
+function changeColorgreen(){
+document.getElementById('mytable').bgColor="#79FF79";
+}
+function changeColoryellow(){
+document.getElementById('mytable').bgColor="#fef670";
+}
+function changeColorpurple(){
+document.getElementById('mytable').bgColor="#DCB5FF";
+}
+function changeColororange(){
+document.getElementById('mytable').bgColor="#FFD1A4";
+}
+
+function reset(){
+document.getElementById('mytable').width="200";
+document.getElementById('mytable').border="1";
+document.getElementById('mytable').bgColor="#fef670";
+}
